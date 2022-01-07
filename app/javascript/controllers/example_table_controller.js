531eeb96af74ff4fcdd0b9fe7c0c044c5bfcbd5d
Cedric Lefebvre <derrioh@gmail.com>
2022-01-07 12:47:54 +0100

    Initial

---
+++ app/javascript/controllers/example_table_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="example-table"
+export default class extends Controller {
+  connect() {
+    //console.log("control")
+    //console.log($("#example"))
+    //$('#example').DataTable()
+  }
+}
